f3d1b83707ec003763e12056cedb6749cf126f19
peayss <pys020831@naver.com>
2024-11-12 22:48:57 +0900

    11/12

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,9 +9,17 @@ function Layout({children}:LayoutProps){
     return(
         <>
             <Header />
-            <main></main>
+            <LayoutStyle>{children}</LayoutStyle>
             <Footer />
         </>
     );
 }
+
+const LayoutStyle = styled.main`
+    width: 100%;
+    margin: 0 auto;
+    max-width: ${({theme}) => theme.layout.width.large};
+    padding: 20px 0;
+
+`
 export default Layout;
